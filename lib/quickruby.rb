f6bb66a0271a19bab24865ed0ed9e958865e762d
Dusan <dusan@netoff.dev>
2023-11-26 11:22:38 +0100

    First build

---
+++ lib/quickruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quickruby/version"
+
+module Quickruby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/quickruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Quickruby
+  VERSION = "0.1.0"
+end
