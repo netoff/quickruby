c7ab1c2498e18becbc4d78d2c61c3788865f1b6c
Dusan <netoff_@outlook.com>
2024-01-12 22:52:07 +0100

    Forgot to uncomment tests

--- tests/test_test_file.rb
+++ tests/test_test_file.rb
@@ -1,42 +1,42 @@
-# tests "it can have multimple tests" do
-#   test_file = <<-TEST_FILE
-#     tests "pass" do
-#       assert true
-#     end
-#
-#     tests "pass1" do
-#       assert true
-#     end
-#   TEST_FILE
-#
-#   test_file_run = TestFile.run(test_file)
-#
-#   assert test_file_run.test_cases.size == 2
-#   assert test_file_run.assertions == 2
-#   assert test_file_run.failures == 0
-# end
-#
-# tests "it can not have multiple tests with same name" do
-#   test_file = <<-TEST_FILE
-#     tests "pass" do
-#       assert true
-#     end
-#
-#     tests "pass" do
-#       assert true
-#     end
-#   TEST_FILE
-#
-#   err = nil
-#
-#   begin
-#     TestFile.run(test_file)
-#   rescue => e
-#     err = e
-#   end
-#
-#   assert err.message == "tests `pass` already defined"
-# end
+test "it can have multimple tests" do
+  test_file = <<-TEST_FILE
+    test "pass" do
+      assert true
+    end
+
+    test "pass1" do
+      assert true
+    end
+  TEST_FILE
+
+  test_file_run = TestFile.run(test_file)
+
+  assert test_file_run.test_cases.size == 2
+  assert test_file_run.assertions == 2
+  assert test_file_run.failures == 0
+end
+
+test "it can not have multiple tests with same name" do
+  test_file = <<-TEST_FILE
+    test "pass" do
+      assert true
+    end
+
+    test "pass" do
+      assert true
+    end
+  TEST_FILE
+
+  err = nil
+
+  begin
+    TestFile.run(test_file)
+  rescue => e
+    err = e
+  end
+
+  assert err.message == "tests `pass` already defined"
+end
 
 test "it count exception within tests as failure" do
   test_file = <<-TEST_FILE
